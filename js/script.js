5be932a31b5964ea07392820ab3fe0a6cea31066
Robert Martyniuk <r.martyniuk1234@gmail.com>
2022-09-17 20:21:49 +0200

    new

--- js/script.js
+++ js/script.js
@@ -41,13 +41,13 @@ const optArticleSelector = '.post',
     optArticleTagsSelector = '.post-tags .list';
 
 
-function generateTitleLinks() {
+function generateTitleLinks(customSelector = '') {
     /* remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = '';
 
     /* find all the articles and save them to variable: articles */
-    const articles = document.querySelectorAll(optArticleSelector);
+    const articles = document.querySelectorAll(optArticleSelector + customSelector);
     let html = '';
 
     for (let article of articles) {
@@ -75,6 +75,7 @@ function generateTitleLinks() {
     }
 }
 generateTitleLinks();
+console.log('wywołano generateTitleLinks: ', generateTitleLinks);
 
 
 /*Dodanie tagów do artykułów*/
@@ -119,3 +120,68 @@ function generateTags() {
     }
 }
 generateTags();
+
+
+function tagClickHandler(event) {
+    /* prevent default action for this event
+    ---zapobiegaj domyślnej akcji dla tego zdarzenia */
+    event.preventDefault();
+
+    /* make new constant named "clickedElement" and give it the value of "this" 
+    ---utwórz nową stałą o nazwie "clickedElement" i nadaj jej wartość "this"*/
+    const clickedElement = this;
+
+    /* make a new constant "href" and read the attribute "href" of the clicked element
+    ---utwórz nową stałą "href" i odczytaj atrybut "href" klikniętego elementu */
+    const href = clickedElement.getAttribute('href');
+
+    /* make a new constant "tag" and extract tag from the "href" constant 
+    ---utwórz nową stałą „tag” i wyodrębnij tag ze stałej „href”*/
+    const tag = href.replace('#tag-', '');
+
+    /* find all tag links with class active
+    ---znajdź wszystkie linki do tagów z aktywną klasą */
+    const activeTagLinks = document.querySelectorAll('a.active[href^="tag-"]');
+
+    /* START LOOP: for each active tag link
+    ---START LOOP: dla każdego aktywnego linku tagu */
+    for (let activeTagLink of activeTagLinks) {
+
+        /* remove class active */
+        activeTagLink.classList.remove('active');
+
+        /* END LOOP: for each active tag link */
+    }
+
+    /* find all tag links with "href" attribute equal to the "href" constant 
+    ---znajdź wszystkie linki do tagów z atrybutem „href” równym stałej „href”*/
+    const tagLinksHref = document.querySelectorAll('a[href="' + href + '"]');
+
+
+    /* START LOOP: for each found tag link
+    ---dla każdego znalezionego linku tagu */
+    for (tagLink of tagLinkHref) {
+
+        /* add class active */
+        tagLink.classList.add('active')
+
+        /* END LOOP: for each found tag link */
+    }
+
+    /* execute function "generateTitleLinks" with article selector as argument */
+    generateTitleLinks('[data-tags~="' + tag + '"]');
+}
+
+
+
+function addClickListenersToTags() {
+    /* find all links to tags */
+
+    /* START LOOP: for each link */
+
+    /* add tagClickHandler as event listener for that link */
+
+    /* END LOOP: for each link */
+}
+
+addClickListenersToTags();
